3a1f27e5479deec3b8e09ce342daf1ce0ebcc44f
baviamu <48151961+baviamu@users.noreply.github.com>
2019-09-05 15:31:41 +0530

    Update pali.py

--- pali.py
+++ pali.py
@@ -1,20 +1,20 @@
 class Stack:
-def __init__(self):
-self.deva = []
-def is_empty(self):
-return self.deva == []
-def push(self, data):
-self.deva.append(data)
-def pop(self):
-return self.deva.pop()
+    def __init__(self):
+        self.deva = []
+    def is_empty(self):
+        return self.deva == []
+    def push(self, data):
+        self.deva.append(data)
+    def pop(self):
+        return self.deva.pop()
 subi= Stack()
-barathi = input('')
+barathi = input(' ') 
 for char in barathi:
-subi.push(char)
-rev_text = "
+    subi.push(char)
+rev_text = ''
 while not subi.is_empty():
-rev_text = rev_text + subi.pop()
+    rev_text = rev_text + subi.pop()
 if barathi== rev_text:
-print(YES)
+    print('YES')
 else:
-print(NO)
+    print('NO')
